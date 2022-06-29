026e4143600d959cdf05aeb8d43987c64cfb3d88
widemar <widemarinternship@gmail.com>
2022-06-29 10:49:50 -0300

    Kotlin Exercícios

--- EstruturaCondicional/Notas.kt
+++ EstruturaCondicional/Notas.kt
@@ -1,3 +1,16 @@
 fun main() {
+    print("Digite a primeira nota: ")
+    val notaUm = readln().toDouble()
+    print("Digite a segunda nota: ")
+    val notaDois = readln().toDouble()
 
+    val somaNotas = notaUm + notaDois
+
+    print(
+        if (somaNotas >= 60) {
+            "Nota final = ${somaNotas.format(1)}".uppercase()
+        } else {
+            "Nota final = ${somaNotas.format(1)}\nReprovado".uppercase()
+        }
+    )
 }
