f80ac86a26694d5785b71c706b100c49e66fb151
widemar <widemarinternship@gmail.com>
2022-06-29 01:32:31 -0300

    Kotlin Exercícios

---
+++ EstruturaCondicional/Troco.kt
@@ -0,0 +1,12 @@
+fun main() {
+    print("Preço unitário do produto: ")
+    val precoUnitario = readln().toDouble()
+    print("Quantidade comprada: ")
+    val quantidadeComprada = readln().toInt()
+    print("Dinheiro recebido: ")
+    val dinheiroRecebido = readln().toDouble()
+
+    val troco = dinheiroRecebido - precoUnitario * quantidadeComprada
+
+    print("Troco = ${troco.format(2)}")
+}
